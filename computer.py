bb5da01ca6803f7cb0545b90e1f0bb1ae191756f
Steve Falcigno <steve1281@hotmail.com>
2019-08-27 09:30:16 -0400

    moved computer AI classes to their own file

---
+++ computer.py
@@ -0,0 +1,86 @@
+from random import randint
+
+class SimpleAI:
+    def __init__(self, game):
+        self.board = game.board
+        self.check_possible = game.check_possible
+        self.solutions = game.solutions
+        self.PLAYER_X = game.PLAYER_X
+        self.PLAYER_O = game.PLAYER_O
+
+    def description(self):
+        return "Simple - random"
+
+    def computer_next_available(self, player):
+        possible = []
+        for x in range(9):
+            if  self.board[x] == x:
+                possible.append(x)
+        if len(possible):
+            r = randint(0, len(possible)-1)
+            self.board[possible[r]] = player
+            return True
+
+    def get_computer_player_input(self, player):
+        if self.computer_next_available(player) : return True
+
+    
+class ComputerAI(SimpleAI):
+
+    def description(self):
+        return "Medium complexity - beatable though"
+
+    def computer_winning_move(self, player):
+        for x in self.solutions:
+            y = self.check_possible(x, player)
+            if y > 0:
+                self.board[y] = player
+                return True
+
+    def computer_blocking_move(self, player):
+        # check for winning moves on the opposing player
+        if player == self.PLAYER_O:
+            check_player = self.PLAYER_X
+        else:
+            check_player = self.PLAYER_O
+
+        # if found, block it and return true.
+        for x in self.solutions:
+            y = self.check_possible(x, check_player)
+            if y > 0:
+                self.board[y] = player
+                return True
+
+    def computer_take_center(self, player):
+        if self.board[4] == 4:
+            self.board[4] = player
+            return True
+
+    def computer_take_corner(self, player):
+        if self.board[0] == 0:
+            self.board[0] = player
+            return True
+        if self.board[2] == 2:
+            self.board[2] = player
+            return True
+        if self.board[6] == 6:
+            self.board[6] = player
+            return True
+        if self.board[8] == 8:
+            self.board[8] = player
+            return True
+
+
+    def get_computer_player_input(self, player):
+        # check if there is a winning move
+        if self.computer_winning_move(player): return True
+        # check if there is a blocking move
+        if self.computer_blocking_move(player): return True
+        # check if there is a center
+        if self.computer_take_center(player): return True
+        # check if there is a corner
+        if self.computer_take_corner(player): return True
+        # choose next available spot
+        if self.computer_next_available(player): return True
+
+
